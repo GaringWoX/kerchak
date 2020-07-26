c49f418cf20006a38f82e4584ab8b06a5646b4ae
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-07-27 01:01:33 +0700

    Add files via upload

---
+++ commands/fwc.js
@@ -0,0 +1,28 @@
+const db = require('quick.db');
+
+module.exports = {
+	name: 'fwc',
+	description: 'fake new member',
+	run: (client, message, args) => {
+    
+    if(!message.member.hasPermission('ADMINISTRATOR')) {
+      return message.reply('You lack of __PERMISSION__ `ADMINISTRATOR`')
+    };
+      
+    if(!db.has(`wchan_${message.guild.id}`)) {
+     return message.channel.send('Please set your __Welcome Channel!__')
+    };
+    
+    if(!db.has(`wgreet_${message.guild.id}`)) {
+      return message.channel.send('Please set your __Welcome Message!__')
+    };
+    
+    if(!db.has(`wimg_${message.guild.id}`)) {
+      return message.channel.send('Please set your __Welcome Message!__')
+    };
+    
+		message.client.emit('guildMemberAdd', message.member);
+      
+  
+  },
+};
