9712bb74e2e2da6e9c2b3114c80a7430d653f1c7
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-04 15:26:44 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,10 +4,13 @@ module.exports = (client) => {
     `with ${client.users.cache.size} users`,
     `at discord.gg/gangsebelah`,
     `at GANG SEBELAH`,
-    `and Making Love`
+    `in ${client.channels.cache.size} channels`,
+    `and Making Love`,
+    `with Your Heart`
     ];
   
 	console.log('Ready!');
+  console.log(actvs);
   setInterval(() => {
         const index = Math.floor(Math.random() * (actvs.length));
         client.user.setActivity(`${actvs[index]}`);
