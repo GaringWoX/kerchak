3bbaaebacad1216b9722a310ae10bd5cc6c5ce7c
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-08 02:42:14 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,7 +5,7 @@ module.exports = async (client, message) => {
   if (message.content.indexOf(client.config.prefix) !== 0) {
      (client.ar[message.content].indexOf("<USER>") != -1)
        message.channel.send(client.ar[message.content].replace("<USER>", `${message.author}`))
-  };
+  }
   
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
