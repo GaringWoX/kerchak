1252ee31958f0d6e614ce866a5d0bba671c35d2a
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-09 03:47:18 +0700

    Create vcuser.js

---
+++ commands/vcuser.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+exports.run = async (client, message, args) => {
+    
+    const vc = (message.guild.voiceStates.cache.size)
+    const vcmbd = new Discord.MessageEmbed()
+    .setColor('#7289da')
+    .setTitle(`Total member in voice channels:\n🔊 ${vc}`)
+    .setTimestamp()
+    message.channel.send(vcmbd);
+
+};
