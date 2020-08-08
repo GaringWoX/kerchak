858a1985e7db5e35f0cf278fd233c117483b76c3
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-09 03:49:39 +0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,10 +4,11 @@ module.exports = (client, guild, message) => {
   
   const gcembed = new Discord.MessageEmbed()
   .setColor('#00ff00')
-  .setDescription(`New guild joined: **${guild.name}** (ID: ${guild.id}). This guild has ${guild.memberCount} members!`)
+  .setTitle('New guild joined:')
+  .setDescription(`**Guild Name:** ${guild.name}\n**Guild ID:** (\`${guild.id}\`)\nThis guild has \`${guild.memberCount}\` members!`)
   .setFooter(`Now connected to ${client.guilds.cache.size} guilds`)
   .setTimestamp()
   
-  client.channels.cache.get(client.comfig.botLogs).send(gcembed);
+  client.channels.cache.get(client.config.botLogs).send(gcembed);
   
 };
