a93c68b80d6764489769394980f3c38354b49ed0
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-09 03:04:52 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,10 +2,36 @@ module.exports = async (client, message) => {
   
   if (!message.guild || message.author.bot) return;
   
-  if (message.content.indexOf(client.config.prefix) !== 0) {
-     (client.ar[message.content].indexOf("<USER>") != -1)
-       message.channel.send(client.ar[message.content].replace("<USER>", `${message.author}`))
-  }
+  const trg = message.content.toLowerCase();
+  const ar = ["welkam", "welcome",
+             "<@300944075983421451>",
+             "<@622047554359656473>",
+             "<@720224674457452594>",
+             "<@508092111858434110>"];
+  
+  if (trg === ar[0]) {
+    message.channel.send("<a:welcomeimage_1:727887823537176638><a:welcomeimage_2:727887823268610089>");
+  } else
+    
+  if (trg === ar[1]) {
+    message.channel.send("<a:wlcm1:741733944130797608><a:wlcm2:741734096710926357>");
+  } else
+    
+  if (trg === ar[2]) {
+    message.reply("Maaf kalau slowrespon, coba WA aja yaa.... <:sinisamaom:731030533169217566>");
+  } else
+  if (trg === ar[3]) {
+    message.reply("Apa sayang? Penting DM aja yuhuuu <a:RainbowWeeb:729649922240151563>");
+  } else
+  if (trg === ar[4]) {
+    message.reply("Iya sayang kenapa?");
+  } else
+  if (trg === ar[5]) {
+    message.reply("Ada apa tag - tag? Kalau kangen DM aja.... <a:ciatciat:725117108832436274>");
+  };
+  
+  
+  if (message.content.indexOf(client.config.prefix) !== 0) return;
   
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
