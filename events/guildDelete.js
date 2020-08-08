bb3033873adb559d8ccfbf061e19b310f2a40207
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-09 03:48:51 +0700

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,7 +4,8 @@ module.exports = (client, guild, message) => {
   
   const gdembed = new Discord.MessageEmbed()
   .setColor('#ff0000')
-  .setDescription(`I have been removed from: **${guild.name}** (ID: ${guild.id})`)
+  .setTitle('I\'ve been removed from:')
+  .setDescription(`**Guild Name:** ${guild.name}\n**Guild ID:** (\`${guild.id}\`)`)
   .setFooter(`Now connected to ${client.guilds.cache.size} guilds`)
   .setTimestamp()
   
