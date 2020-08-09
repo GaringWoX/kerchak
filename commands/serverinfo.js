225bb62d65dc64c9f04155f29ab1e87aaacda992
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-10 06:13:14 +0700

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -10,8 +10,10 @@ exports.run = async (client, message, args) => {
         return days + (days == 1 ? " day" : " days") + " ago";
     };
     
+    let partnered = {"true": "<:yes:741835487114821663> YES", "false": "<:no:741835555310141451> NO"};
+    let emo = {"0": "100", "1": "200", "2": "300", "3": "400"};
+    let verified = {"true": "<:yes:741835487114821663> YES", "false": "<:no:741835555310141451> NO"};
     let verifLevels = {"NONE": "NONE", "LOW": "LOW", "MEDIUM": "MEDIUM", "HIGH": "(╯°□°）╯︵  ┻━┻", "VERY_HIGH": "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"};
-    let presence = {"online": "ngentot"};
     let region = {"brazil": ":flag_br: Brazil",
         "eu-central": ":flag_eu: Central Europe",
         "singapore": ":flag_sg: Singapore",
@@ -34,21 +36,26 @@ exports.run = async (client, message, args) => {
         .setAuthor("SERVER INFO")
         .setColor("#7289da")
         .addField("Guild:", `${message.guild.name}\n(\`${message.guild.id}\`)`, true)
-        .addField("Verification Level:", verifLevels[message.guild.verificationLevel], true)
         .addField("Owner:", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
         .addField("Region:", region[message.guild.region], true)
+        .addField("Partnered | Verified:", `${partnered[message.guild.partnered]} | ${verified[message.guild.verified]}`, true)
+        .addField("Verification Level:", verifLevels[message.guild.verificationLevel], true)
+        .addField("Explicit Content Filter:", `${message.guild.explicitContentFilter}`, true)
         .addField("Banner:", `🔗 [Click Me!](${message.guild.bannerURL()})`, true)
         .addField("Vanity URL:", `🔗 https://discord.gg/${message.guild.vanityURLCode}`, true)
-        .addField("Guild Boost Tier:", `<a:boostgems:739595657123201125> ${message.guild.premiumTier}`, true)
-        .addField("Guild Boost Total:", `<a:boostgems:739595657123201125> ${message.guild.premiumSubscriptionCount}`, true)
-        .addField("Channels | Roles | Emojis:", `${message.guild.channels.cache.size} | ${message.guild.roles.cache.size} | ${message.guild.emojis.cache.size}`, true)
-        .addField("Total | Humans | Bots:", `${message.guild.members.cache.size} | ${message.guild.members.cache.filter(member => !member.user.bot).size} | ${message.guild.members.cache.filter(member => member.user.bot).size}`, true)
+        .addField("Tier:", `<a:boostgems:739595657123201125> Level ${message.guild.premiumTier}`, true)
+        .addField("Boosters:", `<a:boostgems:739595657123201125> ${message.guild.premiumSubscriptionCount}`, true)
+        .addField("Channels | Roles | Emojis:", `${message.guild.channels.cache.size} | ${message.guild.roles.cache.size} | ${message.guild.emojis.cache.size}/${emo[message.guild.premiumTier]}`, true)
+        .addField("AFK Channel | Member Highest Role:", `${message.guild.afkChannel} | ${message.member.roles.highest}`, true)
+        .addField("Member Total | Humans | Bots:", `${message.guild.members.cache.size} | ${message.guild.members.cache.filter(member => !member.user.bot).size} | ${message.guild.members.cache.filter(member => member.user.bot).size}`, true)
         .addField("Member Presences:", `<:online:741196747748933682> ${message.guild.members.cache.filter(member => member.presence.status === "online").size} | <:idle:741197218861678644> ${message.guild.members.cache.filter(member => member.presence.status === "idle").size} | <:dnd:741196524238667846> ${message.guild.members.cache.filter(member => member.presence.status === "dnd").size} | <:offline:741197268123648020> ${message.guild.members.cache.filter(member => member.presence.status === "offline").size}`, true)
         .addField("Member in Voice Channels:", `🔊 ${message.guild.voiceStates.cache.size}`, true)
         .addField("Creation Date:", `${moment(message.guild.createdTimestamp).format("llll")}\n(${checkDays(message.guild.createdAt)})`, true)
+        .addField("You JoinedAt:", `${moment(message.member.joinedTimestamp).format("llll")}\n(${checkDays(message.member.joinedAt)})`, true)
         .setThumbnail(message.guild.iconURL({ dynamic: true }))
         .setFooter(`Requested by: ${message.member.user.username}#${message.member.user.discriminator}`, `${message.member.user.displayAvatarURL({ dynamic: true })}`, true)
         .setTimestamp()
-    message.channel.send({embed});
+    
+     message.channel.send({embed})
   
 };
