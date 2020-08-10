c419cc4781aaa33481753d2bd6c9719835b2583e
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-08-11 05:16:14 +0700

    Update vcuser.js

--- commands/vcuser.js
+++ commands/vcuser.js
@@ -3,11 +3,13 @@ exports.run = async (client, message, args) => {
 
     if(!message.member.hasPermission('MANAGE_CHANNELS')) return;
     
-    const vc = (message.guild.voiceStates.cache.size)
+    message.guild.fetch().then(fetchedGuild => {
+    const totalvcStates = fetchedGuild.voiceStates.cache.size;
+
     const vcmbd = new Discord.MessageEmbed()
     .setColor('#7289da')
     .setTitle(`Total member in voice channels:`)
-    .setDescription(`🔊 ${vc}`)
+    .setDescription(`🔊 ${totalvcStates}`)
     .setTimestamp()
     message.channel.send(vcmbd);
 
